14ef746b6690d6499f4e33f60c87b3df371b850a
Slaytah <thomasedwardwaite@gmail.com>
2022-01-06 15:38:58 +0100

    test token

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 Represents a weighted directed graph (i.e. a collection of nodes that can be
 traversed at some or no cost). This feller can explain it better than I ever
 could: https://www.redblobgames.com/pathfinding/grids/graphs.html, but it's a
-way of representing a map, for example. (or in this case a grid of honeycombs)
+way of representing a map, for example. (or in this case a grid of honeycomb)
 
 Main functionality is get neighbouring nodes, get distance between two nodes, 
 and get cost of moving from one node to another node.
